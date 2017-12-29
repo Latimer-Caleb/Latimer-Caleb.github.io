b67645bff0ba5f46163c36cba384af270fb7c12c
Caleb <nohitokaze@gmail.com>
2017-12-29 02:59:40 -0500

    Almost done with nav, still got some behavior tweaking to do

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 $('.nav a').click(function(){
     $(".nav").find(".active").removeClass("active");
     $(this).parent().addClass("active");
-    // custom colors?? 
+    let newActiveID = $(this).parent().prop('id');
+    console.log(newActiveID);
+    switch(newActiveID){
+        case'AboutNav':
+        // custom colors.
+        console.log('A');
+        break;
+        case'ExpNav':
+        console.log('B');
+        break;
+        case'ProjectsNav':
+        console.log('C');
+        break;
+        case'CourseworkNav':
+        console.log('D');
+        break;
+    }
 });
